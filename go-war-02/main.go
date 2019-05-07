74c01bb61060ab92294878e529ae6e042ce230ad
phuc <hoangphuc735@gmail.com>
2019-05-08 00:12:32 +0700

    8/5

---
+++ go-war-02/main.go
@@ -0,0 +1,48 @@
+package main
+
+func main()  {
+/*	var (
+		name string;
+		adress string;
+		age int;
+	)
+	name = "phuc"
+	adress = "Dong Thap"
+	age  = 1;
+	fmt.Println(name)
+	fmt.Println(adress)
+	fmt.Println(age)*/
+
+/*var myString string = "Phúc"
+
+	runes := []rune(myString)
+	for i:=0; i<len(runes); i++{
+		fmt.Printf("%c",runes[i])
+	}
+*/
+
+//if statement; condition
+	/*if a:= 100; a == 100{
+		fmt.Print("aaa")
+	}*/
+
+//switch - case
+/*	number :=10
+	switch number {
+	case 1:
+		fmt.Print("num")
+		//tiep tuc work
+		fallthrough
+	case 2:
+		fmt.Print("num")
+	case 10:
+		if(number == 10){
+			goto handleNumberEqual10
+	}
+		fmt.Print("num")
+		handleNumberEqual10:
+			fmt.Printf("handle for case :=10")
+	default:
+	}*/
+
+}
